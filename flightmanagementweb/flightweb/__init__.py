42da3241913caa0d9b50ae9207cfef6722fc196e
nmNDdiem <diem.nmngoc@gmail.com>
2024-05-03 01:27:58 +0700

    create models: Airport, FlightRouteType, FlightRoute, Plane, SeatType, Seat, Flight

--- flightmanagementweb/flightweb/__init__.py
+++ flightmanagementweb/flightweb/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.secret_key = "#@^(*&!#)(*)#"
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:123456@localhost/flightdb?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
--- flightmanagementweb/flightweb/index.py
+++ flightmanagementweb/flightweb/index.py
@@ -1,11 +1,16 @@
 from flask import render_template
 
-from flightweb import app
+from flightweb import app, admin
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+# @app.route('/admin')
+# def admin():
+#     return render_template('admin/index.html')
 
 
 if __name__ == '__main__':
