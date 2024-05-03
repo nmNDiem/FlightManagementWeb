90edda8b838cc7863f530014e6e186480ba82827
nmNDdiem <diem.nmngoc@gmail.com>
2024-05-04 00:40:55 +0700

    fix bug: One or more mappers failed to initialize

--- flightmanagementweb/flightweb/admin.py
+++ flightmanagementweb/flightweb/admin.py
@@ -1,14 +1,20 @@
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flightweb import app, db
-from models import FlightRoute, Airport
+from models import FlightRoute, FlightRouteType, Airport, Plane, Flight, Seat, SeatType
 
 
-class FlightRouteView(ModelView):
-    column_list = ['id', 'name', 'departure_airport_id', 'destination_airport_id']
+# class FlightRouteView(ModelView):
+#     column_list = ['id', 'name', 'departure_airport_id', 'destination_airport_id']
 
 
 admin = Admin(app, name='Flight Management Website', template_mode='bootstrap4')
-admin.add_view(FlightRouteView(FlightRoute, db.session))
+# admin.add_view(FlightRouteView(FlightRoute, db.session))
+admin.add_view(ModelView(FlightRoute, db.session))
+admin.add_view(ModelView(FlightRouteType, db.session))
+admin.add_view(ModelView(Flight, db.session))
 admin.add_view(ModelView(Airport, db.session))
+admin.add_view(ModelView(Plane, db.session))
+admin.add_view(ModelView(Seat, db.session))
+admin.add_view(ModelView(SeatType, db.session))
 
